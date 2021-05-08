cf7cf4b7694598e9099336af07dc1b6c89d24653
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-08 17:53:22 +0530

    added : fullDay_wages

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,5 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash
 present=1
+working_hrs=20
+full_day=8
 num=$((RANDOM%2))
 if [ $num -eq 1 ]
 then
@@ -8,4 +10,10 @@ else
          echo " absent"
 fi
 
-
+if [ $num -eq $present ]
+then
+		wages=$((working_hrs * full_day))
+		echo "one  day wages is $wages"
+else
+		echo " absent"
+fi
