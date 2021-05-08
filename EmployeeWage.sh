d913c38a509e77d4fc59f2b3ff19f72033d96d6d
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-08 21:53:24 +0530

    added : employee_switch_case

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,27 +4,31 @@ working_hrs=20
 full_day=8
 partime_hrs=8
 num=$((RANDOM%2))
+
 if [ $num -eq 1 ]
 then
-        echo "empolye is present"
+         echo "empolye is present"
+         echo  " To find the daliy wages 1"
+         echo  "To find on the partime wages only press  2"
+         echo  " To find the full time wages pres 3"
+            read -p "enter the number" n
+            case $n in
+            1)
+                wages=$((working_hrs * full_day))
+            ;;
+            2)
+                partime_wages=$((partime_hrs * working_hrs))
+            ;;
+            3)
+                wages=$((working_hrs * full_day))
+                partime_wages=$((partime_hrs * working_hrs))
+            ;;
+            *)
+               echo "the employe is absent"
+            ;;
+            esac
 else
-         echo " absent"
-fi
-
-if [ $num -eq $present ]
-then
-      wages=$((working_hrs * full_day))
-      #echo "one  day wages is $wages"
-else
-      echo " absent"
-fi
-
-echo "Do u wnt prt time.... enter 1 for Yes..! 0 for No..!!"
-
-read -p "enter the number  :" num2
-if [ $num2 -eq 1 ]
-then
-      partime_wages=$((partime_hrs * working_hrs))
+      echo "employe is absent"
 fi
 total=$(($wages+$partime_wages))
 echo "one  day wages is  : $wages"
