f6bef4f2221b4b0e195b854dfe46ca63ca20a3a8
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-09 22:36:50 +0530

    refactor: created code function

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -62,3 +62,47 @@ do
       totalSalary=$(($totalSalary+$salary));
 done
 echo "onday day  " $salary "20days salary  " $totalSalary
+
+
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && 
+	$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3));
+case $empCheck in
+       	    $isFullTime)
+	    	empHrs=8
+	    	;;
+	    $isPartTime)
+	    	empHrs=4
+		;;
+	    *)
+	    	empHrs=0
+	     	;;
+	esac
+	totalEmpHrs=$(($totalEmpHrs*$empHrs))
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
+
+
+#To Get Work Hours
+getWorkHours() {
+
+        case $empCheck in
+                $isPartTime) 
+			workHours=4
+			;;
+                $isFullTime) 
+			workHours=8;;
+                *) workHours=0;;
+        esac
+        echo $workHours;
+}
